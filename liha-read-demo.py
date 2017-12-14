7dd47a0ce4adff16e5673130746575ead9cb5b14
Jimmy Sastra <jimmy.sastra@gmail.com>
2017-12-14 13:16:20 -0800

    created generic demo that can be run on wc7

---
+++ liha-read-demo.py
@@ -0,0 +1,19 @@
+from autoprotocol.protocol import Protocol
+import json
+
+p = Protocol()
+gg_plate = p.ref("gg_plate", None, "384-flat", storage="ambient")
+tx_plate = p.ref("tx_plate", None, "384-flat", storage="ambient")
+
+p.absorbance(gg_plate, gg_plate.all_wells(),
+             "475:nanometer", "tx_absorbance", flashes=1)
+p.image_plate(tx_plate, mode="top", dataref="tx_image_ng")
+p.transfer(source=tx_plate.well("E19"),
+           dest=tx_plate.well("K12"),
+           volume="8:microliter")
+p.image_plate(tx_plate, mode="top", dataref="tx_image_good")
+
+p.absorbance(tx_plate, tx_plate.all_wells(),
+             "475:nanometer", "gg_absorbance", flashes=1)
+
+print (json.dumps(p.as_dict(), indent=2))
